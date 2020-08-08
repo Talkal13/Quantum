f4bc2ce89f81ac1f3ff79480dde17951880bd80c
Pablo Vazquez <pavazq01@ucm.es>
2020-08-08 20:06:04 +0200

    Boolean operations

---
+++ QCP/aritmetic/boolean.py
@@ -0,0 +1,36 @@
+from qiskit import QuantumCircuit, QuantumRegister
+
+def qnot():
+
+    q = QuantumRegister(1)
+    qc = QuantumCircuit(q)
+    qc.x(q)
+
+    return qc
+
+def xor():
+    q = QuantumRegister(2)
+    qc = QuantumCircuit(q)
+    qc.cx(q[0], q[1])
+
+    return qc
+
+def qand():
+
+    q = QuantumRegister(2)
+    a = QuantumRegister(1)
+    qc = QuantumCircuit(q, a)
+    qc.ccx(q, a)
+
+    return qc
+
+def qor():
+    q = QuantumRegister(2)
+    a = QuantumRegister(1)
+    qc = QuantumCircuit(q, a)
+    qc.x(q)
+    qc.ccx(q, a)
+    qc.x(q)
+    qc.x(a)
+
+    return qc
